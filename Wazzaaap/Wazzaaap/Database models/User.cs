c622eee16880553d1b08329184358c488735a775
Diogo Duarte <100220340+diogod1@users.noreply.github.com>
2022-11-15 20:31:24 +0000

    version 1.1
    Started entity framework

---
+++ Wazzaaap/Wazzaaap/Database models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wazzaaap.Database_models
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+    }
+}
